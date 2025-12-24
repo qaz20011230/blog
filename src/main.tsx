2cc57383a1e6b8eb63eb62fe9b6c2dc493804fdd
Daseinstein <41388546+qaz20011230@users.noreply.github.com>
2025-12-24 21:35:46 +0800

    Fix Buffer polyfill for browser compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import { Buffer } from 'buffer'
+// Polyfill Buffer for the browser
+globalThis.Buffer = Buffer
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
